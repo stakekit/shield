9da6a38a7f61aad6f90c2a21e253827103152869
JAK <akash.jagannathan@gmail.com>
2026-01-22 11:07:17 -0800

    feat: Add language-agnostic JSON interface and CLI for cross-language Shield usage (#2)
    * json schmea, types, handler
    
    * cli
    
    * unit tests
    
    * update readme
    
    * lint
    
    * lint
    
    * args and context passing tests, default operation handler
    
    * lint
    
    * fix: track input byte length
    
    * feat: add cross-platform binary releases with CLI, language examples, and security attestations
    
    * fix: centralized max_input_size, stream cleanup
    
    * fix: lint

---
+++ src/json/index.ts
@@ -0,0 +1,12 @@
+export { handleJsonRequest } from './handler';
+export { MAX_INPUT_SIZE } from './constants';
+export type {
+  JsonRequest,
+  JsonResponse,
+  JsonSuccessResponse,
+  JsonErrorResponse,
+  ValidateResult,
+  IsSupportedResult,
+  GetSupportedYieldIdsResult,
+  ErrorCode,
+} from './types';
